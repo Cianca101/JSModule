1bc86a8f87396ef50911b7f1117fdedea0356965
Cianca101 <emmanuel.carrillo.c@gmail.com>
2020-12-07 22:27:42 -0600

    ADD Variables y Operadores

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 console.log("Hola Koders")
+
+let name_koder = prompt("Cual es tu nomobre?")
+console.log(name_koder)
+
+let age_koder= prompt("Cual es tu edad?")
+console.log(age_koder)
+
+///
+
+let result_suma = parseInt("age_koder") + 5
+console.log(result_suma)
+
+let result_resta = parseInt("age_koder") - 5
+console.log(result_resta)
+
+let result_mult = parseInt("age_koder") * 5
+console.log(result_mult)
+
+let result_div = parseInt("age_koder") / 5
+console.log(result_div)
+
+let result_residuo = parseInt("age_koder") % 5
+console.log(result_residuo)
