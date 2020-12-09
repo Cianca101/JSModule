359af4a5d180c0d08f81ce20cef9d0421b7fb1a2
Cianca101 <emmanuel.carrillo.c@gmail.com>
2020-12-08 21:40:17 -0600

    ADD switch weather

--- js/weather.js
+++ js/weather.js
@@ -1,14 +1,32 @@
 const WEATHER = prompt("Como esta el clima? (Soleado, Lluvioso, Nevando, Nublado, ¿?")
 
-if (WEATHER == "soleado"){
-    console.log(`El día aquí tambien fué ${WEATHER}, deberías tomar agua`)
-} else if(WEATHER == "lluvioso"){
-    console.log(`El día aquí tambien fué ${WEATHER}, deberías usar sombrilla`)
-} else if (WEATHER == "nevando"){
-    console.log(`El día aquí no fué ${WEATHER}, deberías abrigarte`)
-} else if (WEATHER == "nublado"){
-    console.log(`El día aquí tambien fué ${WEATHER}, deberías tomar café`)
-} else {
-    const WEATHER_TWO = prompt("No? entonces como estuvo el clima?")
-    console.log(WEATHER_TWO)
-}
+// if (WEATHER == "soleado"){
+//     console.log(`El día aquí tambien fué ${WEATHER}, deberías tomar agua`)
+// } else if(WEATHER == "lluvioso"){
+//     console.log(`El día aquí tambien fué ${WEATHER}, deberías usar sombrilla`)
+// } else if (WEATHER == "nevando"){
+//     console.log(`El día aquí no fué ${WEATHER}, deberías abrigarte`)
+// } else if (WEATHER == "nublado"){
+//     console.log(`El día aquí tambien fué ${WEATHER}, deberías tomar café`)
+// } else {
+//     const WEATHER_TWO = prompt("No? entonces como estuvo el clima?")
+//     console.log(WEATHER_TWO)
+// }
+
+switch(WEATHER){
+    case `soleado`:
+        console.log(`El día aquí tambien fué ${WEATHER}, deberías tomar agua`)
+        break
+    case `lluvioso`:
+        console.log(`El día aquí tambien fué ${WEATHER}, deberías usar sombrilla`)
+        break
+    case `nevando`:
+        console.log(`El día aquí no fué ${WEATHER}, deberías abrigarte`)
+        break
+    case `nublado`:
+        console.log(`El día aquí tambien fué ${WEATHER}, deberías tomar café`)
+        break
+    default:
+        const WEATHER_TWO = prompt("No? entonces como estuvo el clima?")
+        console.log(WEATHER_TWO)
+    } 
