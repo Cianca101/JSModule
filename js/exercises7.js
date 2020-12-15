b860fdd532913dc4dff67d3ea4a19cda42402b75
Cianca101 <emmanuel.carrillo.c@gmail.com>
2020-12-14 20:15:49 -0600

    ADD Funciones con parametros

--- js/exercises7.js
+++ js/exercises7.js
@@ -1,29 +1,3 @@
-// function multiplicar
-function multiplier_string (numeric_string){
-    for(let i = 1; i <= numeric_string.length; i += 1){
-        for( let m = 1; m <=10; m += 1){
-            let result = numeric_string[i] * m
-            console.log(`${m} x ${numeric_string[i]} = (${result}`)
-        }
-    }
-}
-
-// Palindromo
-function palindrome (palindrome_string){
-    const strng = palindrome_string.split(' ')
-    let reversed = ' '
-
-    for(let i = strng.length -1; i>=0; i--){
-        reversed += strng[i]
-    }
-    console.log(reversed)
-    if(strng === reversed){
-        console.log('Es un palindromo')
-    } else {
-        console.log('No es un palindromo')
-    }
-}
-
 ////////// FUNCIONES CON RETURN ////////////
 
 //Años Caninos con Return
